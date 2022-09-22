eb30fb973d88035e2bcdfb00355d6229d1ae2642
Pierrick Houee <houee@MacBook-Pro.local>
2022-09-22 21:49:57 +0200

    add token check

--- Routes/CreateEvent.js
+++ Routes/CreateEvent.js
@@ -7,13 +7,17 @@ router.post("/create-event", async(req, res) => {
     try {
         const {token, date, title, description} = req.fields
         const account = await Account.find({token: token})
-        const newEvent = new Event({
-            user: account[0],
-            date: date,
-            title: title,
-            description: description
-        }) 
-        await newEvent.save()
+        if(account.length === 0) {
+            res.status(400).json({message: "You need to log in to create an event"})
+        } else {
+            const newEvent = new Event({
+                user: account[0],
+                date: date,
+                title: title,
+                description: description
+            }) 
+            await newEvent.save()
+        }
         res.status(200).json({message: "Event created"})
     } catch (error) {
         res.status(400).json({message: error.message})
